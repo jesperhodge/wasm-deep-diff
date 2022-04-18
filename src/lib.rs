025d84689372fc94790fe524d397583e217e3ddd
Jesper Hodge <jesper.hodge@rtl.de>
2022-04-18 21:34:24 +0200

    add wasm tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    log(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn deep_diff(lhs: &JsValue, rhs: &JsValue) {
+
 }
